088f00b0a1784052367725031a13e3f7b3bea119
Urmila24 <120471055+Urmila24@users.noreply.github.com>
2023-03-07 01:02:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function changecontent(){
+    let x= document.getElementById("know-more").style.display="block";
+    if(x.style.display=="block")
+    {
+        x.style.display="none";
+    }
+    else{
+        x.style.display="block";
+    }
+}
